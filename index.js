1d3d21fe06fa9b73f0ee5e4f317312ef16ad999d
replakcan <alper.makcan@gmail.com>
2025-04-18 20:12:32 +0300

    Overrides Object.toString in Book.prototype
    uses overrided toString function inside of a new created displayAllBooks function and calls it after adding some temporary books.

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ const Book = function (title, author, year) {
   this.year = year
 }
 
+Book.prototype.toString = function () {
+  return `Book info:\nbook_id: ${this.id}\ntitle: ${this.title}\nauthor: ${this.author}\nyear: ${this.year}\n`
+}
+
 const addBookToLibrary = function (title, author, year) {
   const book = new Book(title, author, year)
 
   myLibrary.push(book)
 }
 
+const displayAllBooks = function () {
+  myLibrary.map((book) => console.log(book.toString()))
+}
+
+addBookToLibrary('Guliverin Gezileri', 'Gulmiyver', '2009')
+addBookToLibrary('Dune', 'Frank Herbert', '2001')
+addBookToLibrary('Davincinin Sifresi', 'Dan Brown', '1996')
+addBookToLibrary('Polyanna', 'Oburix', '1954')
+
+displayAllBooks()
