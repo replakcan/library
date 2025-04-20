ee9fa8d7efb2a5654951f066ce8ed808423fa547
replakcan <alper.makcan@gmail.com>
2025-04-20 13:11:40 +0300

    Removes Book related lines from index.js

--- index.js
+++ index.js
@@ -8,24 +8,3 @@ const showButton = document.querySelector('.dialog-show-btn')
 showButton.addEventListener('click', () => {
   dialog.showModal()
 })
-
-Book.prototype.toString = function () {
-  return `Book info:\nbook_id: ${this.id}\ntitle: ${this.title}\nauthor: ${this.author}\nyear: ${this.year}\n`
-}
-
-const addBookToLibrary = function (title, author, year) {
-  const book = new Book(title, author, year)
-
-  myLibrary.push(book)
-}
-
-const displayAllBooks = function () {
-  myLibrary.map((book) => console.log(book.toString()))
-}
-
-addBookToLibrary('Guliverin Gezileri', 'Gulmiyver', '2009')
-addBookToLibrary('Dune', 'Frank Herbert', '2001')
-addBookToLibrary('Davincinin Sifresi', 'Dan Brown', '1996')
-addBookToLibrary('Polyanna', 'Oburix', '1954')
-
-displayAllBooks()
