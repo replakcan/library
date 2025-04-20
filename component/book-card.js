f6eb81229221ea2dacb1a91d1338b1a99df5998f
replakcan <alper.makcan@gmail.com>
2025-04-20 13:13:34 +0300

    Updates BookCard element
    adds a removeBookButton that filters the library with the element which includes the bookId that desired to be removed

--- component/book-card.js
+++ component/book-card.js
@@ -1,6 +1,11 @@
-const createBookCard = function (title, author, year) {
+import { myLibrary, updateLibrary } from './add-book-dialog.js'
+
+const createBookCard = function (id, title, author, year) {
   const bookCard = document.createElement('div')
   bookCard.classList.add('book-card')
+
+  const bookCardId = id.slice(0, 5)
+  bookCard.setAttribute('data-id', bookCardId)
 
   const title_ = document.createElement('h1')
   const author_ = document.createElement('h2')
@@ -13,6 +18,20 @@ const createBookCard = function (title, author, year) {
   bookCard.appendChild(title_)
   bookCard.appendChild(author_)
   bookCard.appendChild(year_)
+
+  const removeBookButton = document.createElement('button')
+  removeBookButton.textContent = 'delete'
+
+  removeBookButton.addEventListener('click', () => {
+    const updatedLibrary = myLibrary.filter((book) => book.id.slice(0, 5) != bookCardId)
+
+    updateLibrary(updatedLibrary)
+
+    const toBeDeletedBookCard = document.querySelector(`[data-id="${bookCardId}"]`)
+
+    toBeDeletedBookCard.remove()
+  })
+  bookCard.appendChild(removeBookButton)
 
   return bookCard
 }
